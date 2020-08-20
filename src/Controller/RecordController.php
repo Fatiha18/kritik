f0d00f1797db8c847afa48240da515d074bb9e18
fatiha <fatihabenbelkacem13@gmail.fr>
2020-08-20 12:06:40 +0200

    Ajoute les pages des albums

--- src/Controller/RecordController.php
+++ src/Controller/RecordController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use App\Entity\Record;
 use App\Repository\ArtistRepository;
+use App\Repository\RecordRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,4 +21,43 @@ class RecordController extends AbstractController
             'artist_list' => $repository->findAll(),
         ]);
     }
+
+     /**
+     * pade d'un artiste
+     * @Route("/artist/{id}", name="artist_page")
+     */
+    public function artistPage(Artist $artist)
+    {
+
+        return $this->render('record/artist_page.html.twig', [
+            'artist' => $artist
+
+        ]);
+    }
+
+    /**
+     * pade d'un album
+     * @Route("/record/{id}", name="record_page")
+     */
+
+     public function recordPage(Record $record)
+     {
+
+        return $this->render('record/record_page.html.twig', [
+            'record' =>$record
+        ]);
+
+     }
+
+  /**
+     * Nouveau album
+     * @Route("/news", name="record_news")
+     */
+    public function recordNews(RecordRepository $repository)
+    {
+        return $this->render('record/record_news.html.twig', [
+               'record_news' => $repository->findNews(),          
+                ]);
+    }
+
 }
